744595afe06a5f0036e1497fb0c6412ccabc17a6
Nasir Khan <virgodgreat@gmail.com>
2020-10-26 10:37:45 +0500

    Auth route tests

--- server/src/tests/Auth/index.test.ts
+++ server/src/tests/Auth/index.test.ts
@@ -3,12 +3,65 @@
 import * as request from 'supertest';
 import app from '../../app';
 
+test('Should create unique user', async () => {
+  await request(app)
+    .post('/auth/register')
+    .send({
+      name: 'Lahori',
+      username: 'lj',
+      email: 'lahori@jawan.com',
+      password: '123456',
+    })
+    .expect(201);
+});
+
+test('Should not create same user', async () => {
+  await request(app)
+    .post('/auth/register')
+    .send({
+      name: 'Lahori',
+      username: 'lj',
+      email: 'lahori@jawan.com',
+      password: '123456',
+    })
+    .expect(400);
+});
+
 test('Should login a user', async () => {
   await request(app)
     .post('/auth/login')
     .send({
-      email: 'dumm@demo.com',
+      email: 'lahori@jawan.com',
+      password: '123456',
+    })
+    .expect(200);
+});
+
+test('Should not login a user that does not exist', async () => {
+  await request(app)
+    .post('/auth/login')
+    .send({
+      email: 'dummy@demo.com',
       password: 'nopass',
+    })
+    .expect(400);
+});
+
+test('Should not create a user with missing credentials', async () => {
+  await request(app)
+    .post('/auth/register')
+    .send({
+      name: 'Lahori',
+      email: 'lahori@jawan.com',
+    })
+    .expect(400);
+});
+
+test('Should not login a user with missing credentials', async () => {
+  await request(app)
+    .post('/auth/login')
+    .send({
+      email: 'lahori@jawan.com',
     })
     .expect(400);
 });
